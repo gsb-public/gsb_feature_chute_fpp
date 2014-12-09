3de04a61fa8a52a15a69f1e3a6a876f295ca1596
Adam Moore <redndahead@gmail.com>
2014-12-08 16:05:09 -0800

    WP-4753: Add icon for validation. Move validation to be more on the server side.

--- js/gsb_feature_chute_fpp.js
+++ js/gsb_feature_chute_fpp.js
@@ -2,28 +2,47 @@
   Drupal.behaviors.gsbFeatureChuteFPP = {
     attach: function (context) {
 
+      // Add icon for validation.
+      $(':input[name="field_link_single[und][0][url]"]').after('<span class="chute-url-icon chute-invalid-url"></span>');
+
+      // Validate the url on load.
+      Drupal.gsbFeatureChuteFPP.validate($(':input[name="field_link_single[und][0][url]"]').val());
+
       // Check for keyup in url field
       $(':input[name="field_link_single[und][0][url]"]').keyup(function(e) {
 
         // Make sure the url is valid
-        var url = $(this).val();
-        if (url.indexOf('admin.getchute.com/#/campaigns/') > 0 && url.indexOf('/displays/') > 0) {
+        Drupal.gsbFeatureChuteFPP.validate($(this).val());
+      });
+    }
+  }
 
-          // Get campaign id and chute id
-          var split1 = url.split('/campaigns/');
-          var data = split1[1].split('/displays/');
+  Drupal.gsbFeatureChuteFPP = Drupal.gsbFeatureChuteFPP || {};
 
-          // Set the values of the fields.
-          $(':input[name="field_chute_id[und][0][value]"]').val(data[1]);
-          $(':input[name="field_chute_campaign_id[und][0][value]"]').val(data[0]);
+  Drupal.gsbFeatureChuteFPP.validate = function (url) {
+    var valid = false;
+    if (url.indexOf('admin.getchute.com/#/campaigns/') > 0 && url.indexOf('/displays/') > 0) {
 
-          // Get the name from the json file on the server.
-          $.getJSON("http://s3.amazonaws.com/store.getchute.com/" + data[1], function( data ) {
-            // Set the name field value.
-            $(':input[name="field_chute_name[und][0][value]"]').val(data.runner);
-          });
+      // Get chute id
+      var data = url.split('/displays/');
+
+      // Check to see if we get a valid response from that url.
+      $.ajax({
+        url: "http://s3.amazonaws.com/store.getchute.com/" + data[1],
+        async: false,
+        dataType: 'json',
+        success: function( data ) {
+          valid = true;
         }
       });
+    }
+
+    // Toggle the icon depending if the url was valid or not.
+    if (valid) {
+      $('span.chute-url-icon').removeClass('chute-invalid-url').addClass('chute-valid-url');
+    }
+    else {
+      $('span.chute-url-icon').addClass('chute-invalid-url').removeClass('chute-valid-url');
     }
   }
 })(jQuery);
