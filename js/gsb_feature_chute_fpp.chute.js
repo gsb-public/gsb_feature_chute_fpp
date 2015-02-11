10e00ff095f7126138028a1102fe0f95aa3c611b
Adam Moore <redndahead@gmail.com>
2015-02-11 08:46:19 -0800

    WP-4966: Use the unique class of the chute fpp instead of the id that doesn't show up when logged out.

--- js/gsb_feature_chute_fpp.chute.js
+++ js/gsb_feature_chute_fpp.chute.js
@@ -2,33 +2,41 @@
   Drupal.behaviors.gsbFeatureChuteFPPChute = {
     attach: function (context) {
       // Find all chute panes
-      $('.gsb-feature-chute-fpp-placeholder:not(.gsb-feature-chute-fpp-placeholder-processed)', context).addClass('gsb-feature-chute-fpp-placeholder-processed').each(function() {
-        // Get some values from the placeholder.
-        var id = $(this).data('chute-id');
-        var name = $(this).data('chute-name');
-        var environment = ($(this).data('chute-environment')) ? $(this).data('chute-environment') + '/' : '';
-        var campaign_id = $(this).data('chute-campaign-id');
+      $('div.pane-bundle-chute:not(.gsb-feature-chute-fpp-processed)', context).addClass('gsb-feature-chute-fpp-placeholder-processed').each(function() {
+        // Get the unique class for this chute pane.
+        var classes = $(this).attr('class').split(' ');
+        for (var i = 0; i < classes.length; i++) {
+          if (classes[i].substring(0, 10) == 'pane-uuid-') {
+            var uniqueClass = classes[i];
+            break
+          }
+        }
 
-        // Need the pane id so we can use this in our non jquery code.
-        var paneID = $(this).closest('div[id^="panels-ipe-paneid-"]').attr('id');
+        // Find the placeholder
+        var placeholder = document.getElementsByClassName(uniqueClass)[0].getElementsByClassName('gsb-feature-chute-fpp-placeholder')[0];
+
+        // Get some values from the placeholder.
+        var id = $(placeholder).data('chute-id');
+        var name = $(placeholder).data('chute-name');
+        var environment = ($(placeholder).data('chute-environment')) ? $(placeholder).data('chute-environment') + '/' : '';
+        var campaign_id = $(placeholder).data('chute-campaign-id');
 
         /**
-         *  This section needs to be pure javascript or it won't load the
-         *  javascript.
+         *  This section needs to be pure javascript or it won't load.
          */
+
         // Create the image element and add it to the page.
         var image = document.createElement('img');
         image.src = 'http://pixel.getchute.com/?dID=' + id + '&cID=' + campaign_id + '&eTp=view_served';
         image.setAttribute('style', 'wdith:0; height:0');
-        document.getElementById(paneID).getElementsByClassName('gsb-feature-chute-fpp-placeholder')[0].appendChild(image);
+        placeholder.appendChild(image);
 
         // Create the script element and add it to the page.
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'http://static.getchute.com/display-runners/' + name + '/' + environment + name + '.js';
         script.setAttribute('data-chute-' + name, 'http://s3.amazonaws.com/store.getchute.com/' + id);
-        document.getElementById(paneID).getElementsByClassName('gsb-feature-chute-fpp-placeholder')[0].appendChild(script);
-
+        placeholder.appendChild(script);
       });
     }
   }
